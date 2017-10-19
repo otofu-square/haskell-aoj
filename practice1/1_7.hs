c84e721d34c8ccf7f5e74cd86bb710a322f87f51
otofu-square <yukita@feedforce.jp>
2017-10-19 22:31:15 +0900

    :+1: Solve practice1 1_7

---
+++ practice1/1_7.hs
@@ -0,0 +1,10 @@
+import Data.Char
+
+rot13 :: Char -> Char
+rot13 x | ord x >= 78 = chr (ord x - 13)
+rot13 x | ord x < 78 = chr (ord x + 13)
+rot13 _ = ' '
+
+main :: IO ()
+main =
+    print $ map rot13 "URYYB"
